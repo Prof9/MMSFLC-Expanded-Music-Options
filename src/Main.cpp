623a56e11ea18cd3d1caeb02385a07b6451446f0
Prof. 9 <prof9@outlook.com>
2026-07-12 17:26:11 +0200

    Implement Favorites option, refactor

--- src/Main.cpp
+++ src/Main.cpp
@@ -955,47 +955,77 @@ extern "C" __declspec(dllexport) bool reframework_plugin_initialize(const REFram
                 s_corePlayBgm_skip = true;
                 return true;
             };
-            auto selectPreferMix = [&](AudioSettingInfo &audioSettingInfo)
+            auto selectMix = [&](AudioSettingInfo &audioSettingInfo)
             {
-                Object playlist = CustomPlaylistMenuItem::getPreferMixPlaylist();
-                Object musicPlayerBgmDefineList = getType("app.sound.SoundMilkyDefine")["MusicPlayerBgmDefineList"];
-                Object bgmSettingDataList = sound["_Manager"]["_BgmSettingDataList"];
-                Type musicPlayerBgmDefineType = getType("app.sound.SoundMilkyDefine.MusicPlayerBgmDefine");
-                reframework::API::Field *musicPlayerBgmDefineType_Series = musicPlayerBgmDefineType.m_type->find_field("Series");
-                reframework::API::Field *musicPlayerBgmDefineType_BgmSettingIndex = musicPlayerBgmDefineType.m_type->find_field("BgmSettingIndex");
+                bool isArranged = sound.get<std::uint8_t>("_CurrentBgmPlayType") == getType("app.cSound_Base").get<std::uint8_t>("BgmPlayType_Arrange");
 
-                /// TODO: Map repeat songs (e.g. Winner! in SF2) to their original trigger IDs
-
-                bool found = false;
-                bool isArranged = false;
-                std::int32_t playlistLen = playlist.get<std::int32_t>("Count");
-                for (std::int32_t i = 0; i < playlistLen; i++)
+                switch (audioSettingInfo.MenuItem->getValue())
                 {
-                    Object favMusicInfo = playlist[i];
-                    std::uint16_t musicId = favMusicInfo.get<std::uint16_t>("musicId");
+                case std::to_underlying(CustomPlaylistMenuItem::Option::AlwaysOriginal):
+                    isArranged = false;
+                    break;
+                case std::to_underlying(CustomPlaylistMenuItem::Option::AlwaysArranged):
+                    isArranged = true;
+                    break;
+                case std::to_underlying(CustomPlaylistMenuItem::Option::PreferMix):
+                    Object playlist = CustomPlaylistMenuItem::getPreferMixPlaylist();
+                    Object musicPlayerBgmDefineList = getType("app.sound.SoundMilkyDefine")["MusicPlayerBgmDefineList"];
+                    Object bgmSettingDataList = sound["_Manager"]["_BgmSettingDataList"];
+                    Type musicPlayerBgmDefineType = getType("app.sound.SoundMilkyDefine.MusicPlayerBgmDefine");
+                    reframework::API::Field *musicPlayerBgmDefineType_Series = musicPlayerBgmDefineType.m_type->find_field("Series");
+                    reframework::API::Field *musicPlayerBgmDefineType_BgmSettingIndex = musicPlayerBgmDefineType.m_type->find_field("BgmSettingIndex");
 
-                    std::uint64_t musicPlayerBgmDefineRaw = musicPlayerBgmDefineList.get<std::uint64_t>(musicId);
-                    std::uint8_t series = *(std::uint8_t *)musicPlayerBgmDefineType_Series->get_data_raw(&musicPlayerBgmDefineRaw, true);
-                    std::uint8_t bgmSettingIndex = *(std::uint8_t *)musicPlayerBgmDefineType_BgmSettingIndex->get_data_raw(&musicPlayerBgmDefineRaw, true);
-
-                    std::uint32_t playerTriggerId = bgmSettingDataList[series]["BgmId2TriggerIdList"][bgmSettingIndex].get<std::uint32_t>("PlayTriggerId");
-
-                    if (triggerId == playerTriggerId)
+                    /// Map repeat songs (e.g. Winner! in SF2) to their original trigger IDs
+                    const static std::unordered_map<std::uint32_t, std::uint32_t> MAP_REPEAT_TRIGGER_IDS = {
+                        {0x412B3875, 0xE994DBDE}, // SF2: Cheerful Indoors -> SF1
+                        {0xC1004ADD, 0x6DFDA90A}, // SF2: Cheerful Company -> SF1
+                        {0xB166E74D, 0x1895189D}, // SF2: Moving Scene -> SF1
+                        {0xA7A6A9EC, 0xE007FD11}, // SF2: Winner! (SF1 Version) -> SF1
+                        {0x24628ADA, 0xA5C48DF3}, // SF2: Winner! - Short (SF1 Version) -> SF1
+                        {0x4360DD04, 0x0DB8091D}, // SF2: Loser -> SF1
+                        {0x93D25171, 0xD19E7AF5}, // SF2: Game Over -> SF1
+                        {0xC2ABB29D, 0x522A7C35}, // SF2: Loneliness -> SF1
+                        {0x295EEC52, 0xE994DBDE}, // SF3: Cheerful Indoors -> SF1
+                        {0xE1AD6054, 0x4C86D9BA}, // SF3: Anthem of the Solitary -> SF2
+                        {0x5448F0F6, 0x6DFDA90A}, // SF3: Cheerful Company -> SF1
+                        {0xA98540F8, 0xBB2D3340}, // SF3: My Friends -> SF2
+                        {0x26957CE4, 0x1895189D}, // SF3: Moving Scene -> SF1
+                        {0x5A095B0E, 0xD19E7AF5}, // SF3: Game Over -> SF1
+                        {0x6ACC1BA0, 0x0DB8091D}, // SF3: Loser... -> SF1
+                    };
+                    uint32_t checkTriggerId = triggerId;
+                    if (auto it = MAP_REPEAT_TRIGGER_IDS.find(triggerId);
+                        it != MAP_REPEAT_TRIGGER_IDS.end())
                     {
-                        found = true;
-                        isArranged = favMusicInfo.get<bool>("isArrange");
+                        checkTriggerId = it->second;
                     }
+
+                    bool found = false;
+                    std::int32_t playlistLen = playlist.get<std::int32_t>("Count");
+                    for (std::int32_t i = 0; i < playlistLen; i++)
+                    {
+                        Object favMusicInfo = playlist[i];
+                        std::uint16_t musicId = favMusicInfo.get<std::uint16_t>("musicId");
+
+                        std::uint64_t musicPlayerBgmDefineRaw = musicPlayerBgmDefineList.get<std::uint64_t>(musicId);
+                        std::uint8_t series = *(std::uint8_t *)musicPlayerBgmDefineType_Series->get_data_raw(&musicPlayerBgmDefineRaw, true);
+                        std::uint8_t bgmSettingIndex = *(std::uint8_t *)musicPlayerBgmDefineType_BgmSettingIndex->get_data_raw(&musicPlayerBgmDefineRaw, true);
+
+                        std::uint32_t playerTriggerId = bgmSettingDataList[series]["BgmId2TriggerIdList"][bgmSettingIndex].get<std::uint32_t>("PlayTriggerId");
+
+                        if (triggerId == playerTriggerId)
+                        {
+                            found = true;
+                            isArranged = favMusicInfo.get<bool>("isArrange");
+                        }
+                    }
+                    break;
                 }
+
+                sound["_Manager"].call("set_EnableBgmArrange", {(void *)(intptr_t)isArranged});
 
                 Object playingBgmInfo = sound["_PlayingBgmInfoList"][playerId];
                 Object playerObject = playingBgmInfo["PlayerObject"];
-
-                if (!found)
-                {
-                    isArranged = sound.get<std::uint8_t>("_CurrentBgmPlayType") == getType("app.cSound_Base").get<std::uint8_t>("BgmPlayType_Arrange");
-                }
-                sound["_Manager"].call("set_EnableBgmArrange", {(void *)(intptr_t)isArranged});
-
                 sound["_ResidentContainer"].call(
                     "trigger(System.UInt32, via.GameObject, via.GameObject, System.UInt32, System.Boolean, System.UInt32, via.simplewwise.CallbackType, System.Action`1<soundlib.SoundManager.RequestInfo>, System.Action`1<soundlib.SoundManager.RequestInfo>, System.Action`1<soundlib.SoundManager.RequestInfo>, System.Action`1<soundlib.SoundManager.RequestInfo>)",
                     {
@@ -1027,9 +1057,14 @@ extern "C" __declspec(dllexport) bool reframework_plugin_initialize(const REFram
                 std::uint8_t bgmPlayType = sound.get<std::uint8_t>("_CurrentBgmPlayType");
                 sound.set<std::uint8_t>("_CurrentBgmPlayType", bgmPlayTypeFavorite);
 
+                bool isCustomPlaylist = audioSettingInfo.MenuItem->getValue() == std::to_underlying(CustomPlaylistMenuItem::Option::Playlist);
+
                 Object saveData = getSingleton("app.Launcher")["_saveData"];
                 Object favoriteMusicList = saveData["favoriteMusicList"];
-                saveData["favoriteMusicList"] = playlistMenuItem->getCustomPlaylist();
+                if (isCustomPlaylist)
+                {
+                    saveData["favoriteMusicList"] = playlistMenuItem->getCustomPlaylist();
+                }
 
                 isRecursiveCall = true;
                 sound.call(
@@ -1042,7 +1077,11 @@ extern "C" __declspec(dllexport) bool reframework_plugin_initialize(const REFram
                 isRecursiveCall = false;
 
                 sound.set<std::uint8_t>("_CurrentBgmPlayType", bgmPlayType);
-                saveData["favoriteMusicList"] = favoriteMusicList;
+
+                if (isCustomPlaylist)
+                {
+                    saveData["favoriteMusicList"] = favoriteMusicList;
+                }
 
                 s_corePlayBgm_retVal = (void *)(intptr_t)getType("app.cSound_Base").get<std::uint8_t>("BgmPlayType_Favorite");
                 s_corePlayBgm_skip = true;
@@ -1102,15 +1141,12 @@ extern "C" __declspec(dllexport) bool reframework_plugin_initialize(const REFram
                             replaced = selectMusicOff(audioSettingInfo);
                             break;
                         case std::to_underlying(CustomPlaylistMenuItem::Option::AlwaysOriginal):
-                            replaced = selectAlwaysOriginal(audioSettingInfo);
-                            break;
                         case std::to_underlying(CustomPlaylistMenuItem::Option::AlwaysArranged):
-                            replaced = selectAlwaysArranged(audioSettingInfo);
-                            break;
                         case std::to_underlying(CustomPlaylistMenuItem::Option::PreferMix):
-                            replaced = selectPreferMix(audioSettingInfo);
+                            replaced = selectMix(audioSettingInfo);
                             break;
                         case std::to_underlying(CustomPlaylistMenuItem::Option::Playlist):
+                        case std::to_underlying(CustomPlaylistMenuItem::Option::Favorites):
                             replaced = selectPlaylist(audioSettingInfo);
                             break;
                         }
